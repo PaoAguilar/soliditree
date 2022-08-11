99a44b1733db042afaac9cf507d42d0ae2e79be3
Paola Aguilar <paola.aguilar@goluuk.com>
2022-08-10 20:53:10 -0600

    mint button validatiion

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React, { useEffect } from "react";
-import { useMoralis, useNFTBalances } from "react-moralis";
+import { useMoralis, useNFTBalances, useERC20Transfers } from "react-moralis";
 import Footer from "../src/components/Footer/Footer";
 import { Navbar } from "../src/components/Navbar/Navbar";
 import { nftAddress } from "../src/web3/addresses";
@@ -19,13 +19,14 @@ const Profile = () => {
     "0xdffeb33a4726dd9fdaa4d05a54d6a1effabbd5f5b2c8cc66c3c1630ef432ff37";
   const { data: hasRole, fetch: fetchCheckRole } = useCheckRole(role, account);
   const { data: nftData, isLoading } = useNFTBalances();
-  const {
-    data: mintData,
-    fetch: safeMint,
-    isLoading: isLoadingMint,
-  } = useSafeMint(account);
+  const { fetch: safeMint, isLoading: isLoadingMint } = useSafeMint(account);
   const { data: balance } = useWalletBalance(account);
   const { data: decimals, fetch: fetchDecimals } = useDecimal();
+  const {
+    fetchERC20Transfers,
+    data: ERC20TransferData,
+    isFetching,
+  } = useERC20Transfers();
 
   const myNftData = nftData?.result?.filter(
     (nftData) => nftData.token_address === nftAddress
@@ -45,6 +46,7 @@ const Profile = () => {
     if (isWeb3Enabled) {
       fetchCheckRole();
       fetchDecimals();
+      fetchERC20Transfers();
     }
   }, [isWeb3Enabled]);
   return (
@@ -76,29 +78,31 @@ const Profile = () => {
             TOTAL STAKE BALANCE
           </h2>
           <h2 className="sm:px-4 text-3xl text-social-impact-100">
-            {walletBalance}
+            {walletBalance || 0}
           </h2>
           <hr className="w-full my-8 border-gray-300" />
         </div>
       </div>
-      <div className="max-w-screen-sm sm:text-center sm:mx-auto">
-        <button
-          className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-social-impact-100 hover:bg-social-impact-200 focus:shadow-outline focus:outline-none"
-          title="withdraw"
-          form="transactions"
-          onClick={(e) => {
-            if (hasRole) {
-              try {
-                safeMint();
-              } catch (e) {
-                console.log(e);
+      {hasRole && (
+        <div className="max-w-screen-sm sm:text-center sm:mx-auto">
+          <button
+            className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-social-impact-100 hover:bg-social-impact-200 focus:shadow-outline focus:outline-none"
+            title="withdraw"
+            form="transactions"
+            onClick={(e) => {
+              if (hasRole) {
+                try {
+                  safeMint();
+                } catch (e) {
+                  console.log(e);
+                }
               }
-            }
-          }}
-        >
-          {isLoadingMint ? "Loading..." : "MINT"}
-        </button>
-      </div>
+            }}
+          >
+            {isLoadingMint ? "Loading..." : "MINT"}
+          </button>
+        </div>
+      )}
       {/* FIN DE LA PRIMERA PARTE */}
       <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="flex flex-col bg-white border rounded shadow-sm md:justify-center lg:flex-row">
@@ -167,14 +171,15 @@ const Profile = () => {
                 />
               </svg>
             </span>
-            MY NFT'S
+            MY NFTS
           </h2>
           <p className="text-base text-gray-700 md:text-lg">
-            NFT's obtained as rewards
+            NFTs obtained as rewards
           </p>
         </div>
         <div className="grid gap-5 row-gap-5 mb-8 lg:grid-cols-4 sm:grid-cols-2">
           {myNftData?.map((nftData) => {
+            console.log("nftData", nftData);
             return (
               <a
                 key={nftData.token_uri}
